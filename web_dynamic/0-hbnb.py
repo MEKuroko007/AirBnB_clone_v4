ede0a10084ba00d9059c135dd358d38bf41c868e
M-ESSALHI <98237827+MEKuroko007@users.noreply.github.com>
2024-02-10 08:53:21 +0000

    Update 0-hbnb.py

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""Flask app to generate complete html page containing location/amenity
-dropdown menus and rental listings"""
+"""
+Flask app to render a webpage with location/amenity dropdown menus and rental listings.
+"""
+
 from flask import Flask, render_template
 from models import storage
 import uuid
+
 app = Flask('web_dynamic')
 app.url_map.strict_slashes = False
 
-
 @app.route('/0-hbnb')
 def display_hbnb():
-    """Generate page with popdown menu of states/cities"""
+    """
+    Generate a webpage with dropdown menus of states/cities and rental listings.
+    """
     states = storage.all('State')
     amenities = storage.all('Amenity')
     places = storage.all('Place')
@@ -21,12 +25,13 @@ def display_hbnb():
                            places=places,
                            cache_id=cache_id)
 
-
 @app.teardown_appcontext
 def teardown_db(*args, **kwargs):
-    """Close database or file storage"""
+    """
+    Close the database or file storage.
+    """
     storage.close()
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
